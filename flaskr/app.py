57dc9837e0bcb399486eb504334dbaa829982b34
sethuramanannamalai <Gerrardno8>
2018-12-27 20:09:43 -0800

    Simple text file search

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/search')
+def search_page():
+    return render_template("search.html")
+
+
+@app.route('/search_file', methods=['POST'])
+def search_file():
+	file_name = request.form['query']
+	return open('../corpus/' + file_name + '.txt', 'r').read()
