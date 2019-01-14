d2ea7d48072c7466ee0a237487f3224023558fdc
sethuramanannamalai <Gerrardno8>
2019-01-13 18:22:54 -0600

    Comments added

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# @app.route(address) - each function here contains the starting point of code for each page
 
+# a sample search page
 @app.route('/search')
 def search_page():
-    return render_template("search.html")
+    return render_template("search.html") # This renders the html page mentioned here
 
-
+# Another sample request
 @app.route('/search_file', methods=['POST'])
 def search_file():
 	file_name = request.form['query']
-	return open('../corpus/' + file_name + '.txt', 'r').read()
+	return open('../corpus/' + file_name + '.txt', 'r').read()  # This returns the required result
